a4a3400fc77b1e7459224e5c45f96ea50294da31
George <egor.kondratev2001@mail.ru>
2020-10-01 15:32:02 +0300

    Create tree.pl

---
+++ kp/tree.pl
@@ -0,0 +1,356 @@
+father(Alex Kondratev, Egor Kondratev).
+mother(Svetlana Larkina, Egor Kondratev).
+father(Alex Kondratev, Eugene Miroy).
+mother(Tanya Kondratev, Eugene Miroy).
+father(Andrew Dofbysh, Kate Dofbysh).
+mother(Svetlana Larkina, Kate Dofbysh).
+father(Alexander Larkin, Svetlana Larkina).
+mother(Ludmila Timofeeva, Svetlana Larkina).
+father(Alexander Larkin, Sergey Larkin).
+mother(Ludmila Timofeeva, Sergey Larkin).
+father(Nikolay Larkin, Alexander Larkin).
+mother(Nadya Pisareva, Alexander Larkin).
+father(Nikolay Larkin, Larisa Larkin).
+mother(Nadya Pisareva, Larisa Larkin).
+father(Sergey Timofeev, Ludmila Timofeeva).
+mother(Antonina Jogina, Ludmila Timofeeva).
+father(Sergey Timofeev, Victor Timofeev).
+mother(Antonina Jogina, Victor Timofeev).
+father(Dmitry Kondratev, Alex Kondratev).
+mother(Elena Beloborodova, Alex Kondratev).
+father(Valery Beloborodov, Elena Beloborodova).
+mother(Nina Beloborodova, Elena Beloborodova).
+father(Nikolay Kondratev, Dmitry Kondratev).
+mother(Unknown Unknown, Dmitry Kondratev).
+father(Yukov Jogin, Antonina Jogina).
+mother(Agrofena Jogina, Antonina Jogina).
+father(Yukov Jogin, Victor Jogin).
+mother(Agrofena Jogina, Victor Jogin).
+father(Yukov Jogin, Valentina Jogina).
+mother(Agrofena Jogina, Valentina Jogina).
+father(Petr Pronichkin, Pavel Pronichkin).
+mother(Valentina Jogina, Pavel Pronichkin).
+father(Petr Pronichkin, Lubov Pronichkin).
+mother(Valentina Jogina, Lubov Pronichkin).
+father(Victor Jogin, Anatoly Jogin).
+mother(Maria Jogina, Anatoly Jogin).
+father(Vova Simkina, Olesy Simkina).
+mother(Lubov Pronichkin, Olesy Simkina).
+father(Victor Timofeev, Irina Timofeeva).
+mother(Svetlana Timofeeva, Irina Timofeeva).
+father(Roman Ataev, Timofey Ataev).
+mother(Irina Timofeeva, Timofey Ataev).
+father(Roman Ataev, Egor Ataev).
+mother(Irina Timofeeva, Egor Ataev).
+father(Stepan Timofeev, Alexandr Timofeev).
+mother(Maria Timofeeva, Alexandr Timofeev).
+father(Stepan Timofeev, Valentina Timofeev).
+mother(Maria Timofeeva, Valentina Timofeev).
+father(Stepan Timofeev, Sergey Timofeev).
+mother(Maria Timofeeva, Sergey Timofeev).
+father(Stepan Timofeev, Nina Timofeeva).
+mother(Maria Timofeeva, Nina Timofeeva).
+father(Stepan Timofeev, Tatyana Timofeev).
+mother(Maria Timofeeva, Tatyana Timofeev).
+father(Stepan Timofeev, Vladimir Timofeev).
+mother(Maria Timofeeva, Vladimir Timofeev).
+father(Nikolay Hlebnikov, Valery Hlebnikov).
+mother(Nina Timofeeva, Valery Hlebnikov).
+father(Victor Nomerovskiy, Sergey Nomerovskiy).
+mother(Tatyana Timofeev, Sergey Nomerovskiy).
+father(Victor Nomerovskiy, Lubov Nomerovskiy).
+mother(Tatyana Timofeev, Lubov Nomerovskiy).
+father(Victor Nomerovskiy, Alena Nomerovskiy).
+mother(Tatyana Timofeev, Alena Nomerovskiy).
+father(Vladimir Timofeev, Alexandr Timofeev).
+mother(Nataly Timofeeva, Alexandr Timofeev).
+father(Vladimir Timofeev, Alex Timofeev).
+mother(Nataly Timofeeva, Alex Timofeev).
+father(Alexandr Timofeev, Dmitriy Timofeev).
+mother(Albina Timofeeva, Dmitriy Timofeev).
+father(Valentin Banit, Alexandr Banit).
+mother(Valentina Timofeev, Alexandr Banit).
+father(Valentin Banit, Alena Banit).
+mother(Valentina Timofeev, Alena Banit).
+father(Roman Banit, Yaroslav Banit).
+mother(Alena Banit, Yaroslav Banit).
+father(Roman Banit, Vaselisa Banit).
+mother(Alena Banit, Vaselisa Banit).
+father(Pavel Volosnik, Andrew Volosnik).
+mother(Larisa Larkin, Andrew Volosnik).
+father(Pavel Volosnik, Alexandr Volosnik).
+mother(Larisa Larkin, Alexandr Volosnik).
+father(Andrew Volosnik, Alina Volosnik).
+mother(Natalya Volosnik, Alina Volosnik).
+father(Alexandr Volosnik, Nastya Volosnik).
+mother(Anna Volosnik, Nastya Volosnik).
+father(Alexandr Volosnik, Alena Volosnik).
+mother(Anna Volosnik, Alena Volosnik).
+father(Alexandr Volosnik, Esenia Volosnik).
+mother(Anna Volosnik, Esenia Volosnik).
+father(Alexandr Volosnik, Alexandra Volosnik).
+mother(Anna Volosnik, Alexandra Volosnik).
+father(Akim Pisarev, Elizabeth Pisarev).
+mother(Elena Soldatova, Elizabeth Pisarev).
+father(Akim Pisarev, Michail Pisarev).
+mother(Elena Soldatova, Michail Pisarev).
+father(Akim Pisarev, Nina Pisarev).
+mother(Elena Soldatova, Nina Pisarev).
+father(Akim Pisarev, Polina Pisarev).
+mother(Elena Soldatova, Polina Pisarev).
+father(Akim Pisarev, Nadya Pisareva).
+mother(Elena Soldatova, Nadya Pisareva).
+father(Akim Pisarev, Alexandra Pisarev).
+mother(Elena Soldatova, Alexandra Pisarev).
+father(Volody Martinov, Alexandr Martinov).
+mother(Nina Pisarev, Alexandr Martinov).
+father(Volody Martinov, Ludmila Martinov).
+mother(Nina Pisarev, Ludmila Martinov).
+father(Volody Martinov, Victor Martinov).
+mother(Nina Pisarev, Victor Martinov).
+father(Ivan Brynchin, Kolya Brynchin).
+mother(Alexandra Pisarev, Kolya Brynchin).
+father(Ivan Brynchin, Lubov Brynchin).
+mother(Alexandra Pisarev, Lubov Brynchin).
+father(Ivan Brynchin, Anatoly Brynchin).
+mother(Alexandra Pisarev, Anatoly Brynchin).
+father(Michail Shishelov, Nadya Shishelov).
+mother(Lubov Brynchin, Nadya Shishelov).
+father(Michail Shishelov, Alex Shishelov).
+mother(Lubov Brynchin, Alex Shishelov).
+father(Andreas Nikaladean, Kolya Nikaladean).
+mother(Nadya Shishelov, Kolya Nikaladean).
+father(Yakov Pronichkin, Kolya Pronichkin).
+mother(Elizabeth Pisarev, Kolya Pronichkin).
+father(Yakov Pronichkin, Petr Pronichkin).
+mother(Elizabeth Pisarev, Petr Pronichkin).
+father(Petr Pronichkin, Sergey Pronichkin).
+mother(Nadya Pronichkina, Sergey Pronichkin).
+father(Kolya Pronichkin, Roman Pronichkin).
+mother(Natalya Pronichkina, Roman Pronichkin).
+father(Anatoly Brynchin, Elena Brynchin).
+mother(Ludmila Brynchin, Elena Brynchin).
+father(Anatoly Brynchin, Kate Brynchin).
+mother(Ludmila Brynchin, Kate Brynchin).
+father(Michail Pisarev, Nina Pisarev).
+mother(Raisa Pisareva, Nina Pisarev).
+father(Kolya Suhar, Nadya Suhar).
+mother(Nina Pisarev, Nadya Suhar).
+father(Kolya Suhar, Kate Suhar).
+mother(Nina Pisarev, Kate Suhar).
+father(Sergey Ivanov, Andrew Ivanov).
+mother(Polina Pisarev, Andrew Ivanov).
+father(Victor Martinov, Ludmila Martinov).
+mother(Anna Martinova, Ludmila Martinov).
+father(Victor Martinov, Elena Martinov).
+mother(Anna Martinova, Elena Martinov).
+father(Sergey Martinov, Anna Martinov).
+mother(Ludmila Martinov, Anna Martinov).
+father(Sergey Martinov, Alexandr Martinov).
+mother(Ludmila Martinov, Alexandr Martinov).
+father(Ilya Larkin, Yakov Larkin).
+mother(Milanya Larkina, Yakov Larkin).
+father(Ilya Larkin, Gregoriy Larkin).
+mother(Milanya Larkina, Gregoriy Larkin).
+father(Ilya Larkin, Nikolay Larkin).
+mother(Milanya Larkina, Nikolay Larkin).
+father(Ilya Larkin, Vladimir Larkin).
+mother(Milanya Larkina, Vladimir Larkin).
+father(Ilya Larkin, Serafima Larkin).
+mother(Milanya Larkina, Serafima Larkin).
+father(Ilya Larkin, Boris Larkin).
+mother(Milanya Larkina, Boris Larkin).
+father(Ilya Larkin, Victor Larkin).
+mother(Milanya Larkina, Victor Larkin).
+father(Boris Larkin, Vladimir Larkin).
+mother(Alexandra Larkina, Vladimir Larkin).
+father(Boris Larkin, Nikolay Larkin).
+mother(Alexandra Larkina, Nikolay Larkin).
+father(Boris Larkin, Sergey Larkin).
+mother(Alexandra Larkina, Sergey Larkin).
+father(Boris Larkin, Anatoly Larkin).
+mother(Alexandra Larkina, Anatoly Larkin).
+father(Boris Larkin, Anna Larkin).
+mother(Alexandra Larkina, Anna Larkin).
+father(Boris Larkin, Ludmila Larkin).
+mother(Alexandra Larkina, Ludmila Larkin).
+father(Boris Larkin, Valentina Larkin).
+mother(Alexandra Larkina, Valentina Larkin).
+father(Anatoly Larkin, Anton Larkin).
+mother(Natalya Larkina, Anton Larkin).
+father(Anatoly Larkin, Svetlana Larkin).
+mother(Natalya Larkina, Svetlana Larkin).
+father(Ivan Larkin, Ludmila Larkin).
+mother(Serafima Larkin, Ludmila Larkin).
+father(Ivan Larkin, Anatoly Larkin).
+mother(Serafima Larkin, Anatoly Larkin).
+father(Nikolay Hrykov, Anna Hrykov).
+mother(Ludmila Larkin, Anna Hrykov).
+father(Sergey Pimanov, Galina Pimanov).
+mother(Anna Hrykov, Galina Pimanov).
+father(Victor Larkin, Gregoriy Larkin).
+mother(Anastasya Larkin, Gregoriy Larkin).
+father(Victor Larkin, Ilya Larkin).
+mother(Anastasya Larkin, Ilya Larkin).
+father(Yakov Larkin, Anna Larkin).
+mother(Natalya Larkin, Anna Larkin).
+father(Yakov Larkin, Alex Larkin).
+mother(Natalya Larkin, Alex Larkin).
+father(Yakov Larkin, Galina Larkin).
+mother(Natalya Larkin, Galina Larkin).
+father(Eugene Miroy, Elena Miroy).
+mother(Yana Miroy, Elena Miroy).
+sex(Egor Kondratev, M).
+sex(Alex Kondratev, M).
+sex(Svetlana Larkina, F).
+sex(Alex Kondratev, M).
+sex(Svetlana Larkina, F).
+sex(Unknown Unknown, F).
+sex(Yana Miroy, F).
+sex(Andrew Dofbysh, M).
+sex(Kate Dofbysh, F).
+sex(Ludmila Timofeeva, F).
+sex(Alexander Larkin, M).
+sex(Nikolay Larkin, M).
+sex(Nadya Pisareva, F).
+sex(Sergey Timofeev, M).
+sex(Elena Beloborodova, F).
+sex(Valery Beloborodov, M).
+sex(Nina Beloborodova, F).
+sex(Dmitry Kondratev, M).
+sex(Nikolay Kondratev, M).
+sex(Unknown Unknown, F).
+sex(Antonina Jogina, F).
+sex(Sergey Larkin, M).
+sex(Agrofena Jogina, F).
+sex(Yukov Jogin, M).
+sex(Victor Jogin, M).
+sex(Valentina Jogina, F).
+sex(Petr Pronichkin, M).
+sex(Lubov Pronichkin, F).
+sex(Pavel Pronichkin, M).
+sex(Maria Jogina, F).
+sex(Anatoly Jogin, M).
+sex(Vova Simkina, M).
+sex(Olesy Simkina, M).
+sex(Victor Timofeev, M).
+sex(Svetlana Timofeeva, F).
+sex(Irina Timofeeva, F).
+sex(Roman Ataev, M).
+sex(Timofey Ataev, M).
+sex(Egor Ataev, M).
+sex(Stepan Timofeev, M).
+sex(Maria Timofeeva, F).
+sex(Nina Timofeeva, F).
+sex(Nikolay Hlebnikov, M).
+sex(Valery Hlebnikov, M).
+sex(Tatyana Timofeev, F).
+sex(Victor Nomerovskiy, M).
+sex(Sergey Nomerovskiy, M).
+sex(Lubov Nomerovskiy, F).
+sex(Alena Nomerovskiy, F).
+sex(Vladimir Timofeev, M).
+sex(Nataly Timofeeva, F).
+sex(Alexandr Timofeev, M).
+sex(Alex Timofeev, M).
+sex(Alexandr Timofeev, M).
+sex(Albina Timofeeva, F).
+sex(Dmitriy Timofeev, M).
+sex(Valentina Timofeev, F).
+sex(Valentin Banit, M).
+sex(Alexandr Banit, M).
+sex(Alena Banit, F).
+sex(Roman Banit, M).
+sex(Yaroslav Banit, M).
+sex(Vaselisa Banit, F).
+sex(Larisa Larkin, F).
+sex(Pavel Volosnik, M).
+sex(Andrew Volosnik, M).
+sex(Alexandr Volosnik, M).
+sex(Natalya Volosnik, F).
+sex(Alina Volosnik, F).
+sex(Anna Volosnik, F).
+sex(Nastya Volosnik, F).
+sex(Alena Volosnik, F).
+sex(Esenia Volosnik, F).
+sex(Alexandra Volosnik, F).
+sex(Akim Pisarev, M).
+sex(Elena Soldatova, F).
+sex(Alexandra Pisarev, F).
+sex(Elizabeth Pisarev, F).
+sex(Michail Pisarev, M).
+sex(Nina Pisarev, F).
+sex(Polina Pisarev, F).
+sex(Volody Martinov, M).
+sex(Alexandr Martinov, M).
+sex(Ludmila Martinov, F).
+sex(Victor Martinov, M).
+sex(Ivan Brynchin, M).
+sex(Kolya Brynchin, M).
+sex(Lubov Brynchin, F).
+sex(Anatoly Brynchin, M).
+sex(Michail Shishelov, M).
+sex(Nadya Shishelov, F).
+sex(Alex Shishelov, M).
+sex(Andreas Nikaladean, M).
+sex(Kolya Nikaladean, M).
+sex(Yakov Pronichkin, M).
+sex(Kolya Pronichkin, M).
+sex(Petr Pronichkin, M).
+sex(Nadya Pronichkina, F).
+sex(Sergey Pronichkin, M).
+sex(Natalya Pronichkina, F).
+sex(Roman Pronichkin, M).
+sex(Ludmila Brynchin, F).
+sex(Elena Brynchin, F).
+sex(Kate Brynchin, F).
+sex(Raisa Pisareva, F).
+sex(Nina Pisarev, F).
+sex(Kolya Suhar, M).
+sex(Kate Suhar, F).
+sex(Nadya Suhar, F).
+sex(Sergey Ivanov, M).
+sex(Andrew Ivanov, M).
+sex(Anna Martinova, F).
+sex(Ludmila Martinov, F).
+sex(Elena Martinov, F).
+sex(Sergey Martinov, M).
+sex(Anna Martinov, F).
+sex(Alexandr Martinov, M).
+sex(Ilya Larkin, M).
+sex(Milanya Larkina, F).
+sex(Vladimir Larkin, M).
+sex(Serafima Larkin, F).
+sex(Boris Larkin, M).
+sex(Victor Larkin, M).
+sex(Yakov Larkin, M).
+sex(Gregoriy Larkin, M).
+sex(Alexandra Larkina, F).
+sex(Vladimir Larkin, M).
+sex(Nikolay Larkin, M).
+sex(Sergey Larkin, M).
+sex(Anatoly Larkin, M).
+sex(Anna Larkin, F).
+sex(Ludmila Larkin, F).
+sex(Valentina Larkin, F).
+sex(Natalya Larkina, F).
+sex(Svetlana Larkin, F).
+sex(Anton Larkin, M).
+sex(Ivan Larkin, M).
+sex(Ludmila Larkin, F).
+sex(Anatoly Larkin, M).
+sex(Nikolay Hrykov, M).
+sex(Anna Hrykov, F).
+sex(Sergey Pimanov, M).
+sex(Galina Pimanov, F).
+sex(Anastasya Larkin, F).
+sex(Gregoriy Larkin, M).
+sex(Ilya Larkin, M).
+sex(Natalya Larkin, F).
+sex(Anna Larkin, F).
+sex(Alex Larkin, M).
+sex(Galina Larkin, F).
+sex(Tanya Kondratev, F).
+sex(Eugene Miroy, M).
+sex(Yana Miroy, F).
+sex(Elena Miroy, F).
