28779a773577c8ded217ced507a4fdc8413e12a2
George <egor.kondratev2001@mail.ru>
2020-10-12 22:39:40 +0300

    Update task2.pl

--- lab1/task2.pl
+++ lab1/task2.pl
@@ -1,6 +1,65 @@
+%%%%%%%%%%%%%%%%%%%%%%%
+%      by Dukend      %  
+%%%%%%%%%%%%%%%%%%%%%%%
 % Task 2: Relational Data
 
-% The line below imports the data
-:- ['one.pl'].
+:- ['two.pl'].
+%:- encoding(utf8).
 
-group(X,L) :- findall(Z,student(X,Z),L).
+
+withoutcopy([], []):-!.
+withoutcopy([X|L], List):-
+          member(X, L),
+	  !, 
+	  withoutcopy(L, List).
+withoutcopy([X|L], [X|List]):-
+	  !, 
+	  withoutcopy(L, List).
+%######################
+% пункт 1 Напечатать средний балл для каждого предмета
+task1():-
+	findall(Sub, grade(_, _, Sub, _), S),
+	withoutcopy(S, Subj),
+	mark(Subj).
+sum([],0).
+sum([M|Marks],Sum):-
+	sum(Marks,S),
+	Sum is M + S.
+mark([]).
+mark([Subj|T]):-
+	findall(Mark,grade(_,_,Subj,Mark),Marks),
+	sum(Marks,M),
+	length(Marks,Len),
+	X is M / Len,
+	write(Subj), write(': '), write(X), nl,
+	mark(T).
+
+%######################
+%  пункт 2 Для каждой группы, найти количество не сдавших студентов
+task2():-
+	findall(Gr, grade(Gr, _, _, _), Group),
+	withoutcopy(Group, G),
+	pass(G).
+pass([]).
+pass([Group|T]):-
+	findall(Stud,grade(Group,Stud,_,2),Studs),
+	withoutcopy(Studs,S),
+	length(S,X),
+	write(Group), write(': '),write(X), nl,
+	pass(T).
+
+%######################
+% пункт 3 Найти количество не сдавших студентов для каждого из предметов
+task3():-
+	findall(Sub, grade(_, _, Sub, _), S),
+	withoutcopy(S, Subj),
+	subj(Subj).
+subj([]).
+subj([Subj|T]):-
+	findall(Stud,grade(_,Stud,Subj,2),Studs),
+	length(Studs,X),
+	write(Subj), write(': '), write(X), nl,
+	subj(T).
+
+%######################
+
